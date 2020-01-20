37c54fe403f45065a4023095812c97a800c8ff40
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-20 13:43:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,8 +37,9 @@ class CashRegister
 
 
   def void_last_transaction()
+    @total -= @last_transaction_amount
     
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.last_transaction_amount
     
   end
 
